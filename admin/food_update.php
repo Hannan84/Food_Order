2bf3a6fcea95901c8da80b0cfdd68c1d79bcde55
Hannan84 <hannansarkar84@gamil.com>
2021-10-04 23:41:06 +0600

    I have modified some line to food_update.php file

--- admin/food_update.php
+++ admin/food_update.php
@@ -16,86 +16,77 @@ if (isset($_POST['submit'])){
 
 ?>
 
-<div class="main_container">
-    <div class="container form_center">
-        <?php if (isset($_SESSION['not_update'])){?>
-            <div class="alert-warning">
-                <strong><?php echo $_SESSION['not_update'];?></strong> Failed to Update.
-            </div>
-            <?php unset($_SESSION['not_update']); }?>
-        <br/>
-        <h1>Update Food</h1>
-        <br/>
-        <form action="" method="post" enctype="multipart/form-data">
-            <table class="tbl-30">
-                <tr>
-                    <td>Title: </td>
-                    <td><input class="form_input" type="text" name="title" value="<?php echo $std['title'];?>"></td>
-                </tr>
-                <tr>
-                    <td>Description: </td>
-                    <td><textarea class="form_input" cols="30" rows="5" name="description"><?php echo $std['description'];?></textarea></td>
-                </tr>
-                <tr>
-                    <td>Price: </td>
-                    <td><input class="form_input" type="text" name="price" value="<?php echo $std['price'];?>"></td>
-                </tr>
-                <tr>
-                    <td>Current Image: </td>
-                    <td>
-                        <?php
-//                      Check whether image is available or not
-                        if ($std['image'] != ""){
-                            ?>
-                            <img src="../images/food/<?php echo $std['image'];?>"width="100px" height="100px">
-                            <?php
-                        }
-                        else{
-                            echo "<div style='color: red'> Image Not Added</div>";
-                        }
-                        ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>New Image: </td>
-                    <td><input class="form_input" type="file" name="image"></td>
-                </tr>
-                <tr>
-                    <td>Category: </td>
-                    <td>
-                        <select name="category_id" class="form_input">
-                            <?php
-                            $obj = new Food();
-                            $data = $obj->activeCategory();
-                            foreach ($data as $value){?>
-                                <option <?php if ($std['category_id'] == $value['id']){ echo "Selected";}?> value="<?php echo $value['id'];?>"><?php echo $value['title'];?></option>
-                            <?php }?>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Feature: </td>
-                    <td class="form_input">
-                        <input <?php if($std['featured'] == 'Yes'){echo "checked";}?> type="radio" name="featured" value="Yes"> Yes
-                        <input <?php if($std['featured'] == 'No'){echo "checked";}?> type="radio" name="featured" value="No"> No
-                    </td>
-                </tr>
-                <tr>
-                    <td>Active: </td>
-                    <td class="form_input">
-                        <input <?php if($std['active'] == 'Yes'){echo "checked";}?> type="radio" name="active" value="Yes"> Yes
-                        <input <?php if($std['active'] == 'No'){echo "checked";}?> type="radio" name="active" value="No"> No
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <input type="hidden" name="current_image" value="<?php echo $std['image']?>">
-                        <input type="submit" name="submit" value="Update-Food" class="btn-primary">
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </div>
+<div class="form-container">
+    <h1 class="text-center">Update Food</h1>
+    <br/>
+    <?php if (isset($_SESSION['not_update'])){?>
+        <div class="alert-warning">
+            <strong><?php echo $_SESSION['not_update'];?></strong> Failed to Update.
+        </div>
+        <?php unset($_SESSION['not_update']); }?>
+    <br/>
+    <form action="" method="post" enctype="multipart/form-data">
+        <br>
+        <div class="form-group">
+            <label for="Title">Title: <i class="fas fa-signature"></i></label>
+            <input required type="text" class="form-control" name="title" value="<?php echo $std['title'];?>">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Description">Description: <i class="fas fa-signature"></i></label>
+            <textarea cols="43" rows="6" name="description"><?php echo $std['description'];?></textarea>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Price">Price: <i class="fas fa-coins"></i></label>
+            <input required type="number" class="form-control" name="price" value="<?php echo $std['price'];?>">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Current Image">Current Image: <i class="fas fa-image"></i></label><br>
+            <?php
+//                  Check whether image is available or not
+                if ($std['image'] != ""){
+                    ?>
+                    <img src="../images/food/<?php echo $std['image'];?>"width="100px" height="100px">
+                    <?php
+                }
+                else{
+                    echo "<div style='color: red'> Image Not Added</div>";
+                }
+            ?>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="New Image">New Image: <i class="fas fa-image"></i></label>
+            <input class="form-control" type="file" name="image">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Category">Category: <i class="fas fa-th-list"></i></label>
+            <select name="category_id" class="form-control">
+                <?php
+                $obj = new Food();
+                $data = $obj->activeCategory();
+                foreach ($data as $value){?>
+                    <option <?php if ($std['category_id'] == $value['id']){ echo "Selected";}?> value="<?php echo $value['id'];?>"><?php echo $value['title'];?></option>
+                <?php }?>
+            </select>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Feature">Feature: <i class="fas fa-cogs"></i></label>
+            <input <?php if($std['featured'] == 'Yes'){echo "checked";}?> type="radio" name="featured" value="Yes"> Yes
+            <input <?php if($std['featured'] == 'No'){echo "checked";}?> type="radio" name="featured" value="No"> No
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="Active">Active: <i class="far fa-thumbs-up"></i></label>
+            <input <?php if($std['active'] == 'Yes'){echo "checked";}?> type="radio" name="active" value="Yes"> Yes
+            <input <?php if($std['active'] == 'No'){echo "checked";}?> type="radio" name="active" value="No"> No
+        </div>
+        <button type="submit" name="submit" class="btn-primary-login">Save</button>
+    </form>
 </div>
 
 <!-- include footer section-->
