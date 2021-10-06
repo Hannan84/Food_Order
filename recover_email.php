10f3c5522d22101416be6554ad04de418352e7d4
Hannan84 <hannansarkar84@gamil.com>
2021-10-06 17:47:19 +0600

    added footer section

--- recover_email.php
+++ recover_email.php
@@ -73,3 +73,9 @@ if (isset($_POST['submit'])){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
+
+    <!--Include social section-->
+<?php include 'partial_front/social.php'?>
+
+    <!--include footer section-->
+<?php include 'partial_front/footer.php'?>
