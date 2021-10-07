4ab0491f0b66d027f9d7c6da6c7550d7b11dad50
Hannan84 <hannansarkar84@gamil.com>
2021-10-07 10:05:52 +0600

    modified some line

--- user_profile.php
+++ user_profile.php
@@ -20,55 +20,54 @@ if (isset($_POST['pass_submit'])){
     $obj->changePassword();
 }
 ?>
-<div class="main_container">
-    <div class="form-container" style="width: 35% ; border: none">
-        <h3 class="text-center" style="border-bottom: 1px solid #d4d3d3; padding-bottom: 25px">My Profile</h3>
+<div class="form-container" style="width: 35% ; border: none">
+    <h3 class="text-center" style="border-bottom: 1px solid #d4d3d3; padding-bottom: 25px">My Profile</h3>
+    <br/>
+    <?php if (isset($_SESSION['not_update'])){?>
+        <div class="alert-warning">
+            <strong><?php echo $_SESSION['not_update'];?></strong> Failed to Update.
+        </div>
+    <?php unset($_SESSION['not_update']); }?>
+    <form action="" method="post">
+        <div class="order-label">
+            <label for="First Name"">First Name: <i class="fas fa-signature"></i></label>
+            <input required type="text" class="input-responsive" name="first_name" placeholder="First Name">
+        </div>
+        <div class="order-label">
+            <label for="Last Name"">Last Name: <i class="fas fa-signature"></i></label>
+            <input required type="text" class="input-responsive" name="last_name" placeholder="Last Name">
+        </div>
+        <div class="order-label">
+            <label for="Email"">Email: <i class="fas fa-mail-bulk"></i></label>
+            <input required type="text" class="input-responsive" name="email" placeholder="Email">
+        </div>
+        <div class="order-label">
+            <label for="Number"">Number: <i class="fas fa-phone"></i></label>
+            <input required type="number" class="input-responsive" name="number" placeholder="Number">
+        </div>
+        <div class="order-label">
+            <label for="Address"">Address: <i class="fas fa-map-marker-alt"></i></label>
+            <textarea required name="address" class="input-responsive" cols="43" rows="7" placeholder="Address"></textarea>
+        </div>
+        <button type="submit" name="submit" class="btn-primary-login">Save</button>
+    </form>
+    <br/><br/><br/><br/>
+    <h3 class="text-center" style="border-bottom: 1px solid #d4d3d3; padding-bottom: 25px">Password</h3>
+    <form action="" method="post">
         <br/>
-        <?php if (isset($_SESSION['not_update'])){?>
-            <div class="alert-warning">
-                <strong><?php echo $_SESSION['not_update'];?></strong> Failed to Update.
-            </div>
-        <?php unset($_SESSION['not_update']); }?>
-        <form action="" method="post">
-            <div class="order-label">
-                <label for="First Name"">First Name: <i class="fas fa-signature"></i></label>
-                <input required type="text" class="input-responsive" name="first_name" placeholder="First Name">
-            </div>
-            <div class="order-label">
-                <label for="Last Name"">Last Name: <i class="fas fa-signature"></i></label>
-                <input required type="text" class="input-responsive" name="last_name" placeholder="Last Name">
-            </div>
-            <div class="order-label">
-                <label for="Email"">Email: <i class="fas fa-mail-bulk"></i></label>
-                <input required type="text" class="input-responsive" name="email" placeholder="Email">
-            </div>
-            <div class="order-label">
-                <label for="Number"">Number: <i class="fas fa-phone"></i></label>
-                <input required type="number" class="input-responsive" name="number" placeholder="Number">
-            </div>
-            <div class="order-label">
-                <label for="Address"">Address: <i class="fas fa-map-marker-alt"></i></label>
-                <textarea required name="address" class="input-responsive" cols="43" rows="7" placeholder="Address"></textarea>
-            </div>
-            <button type="submit" name="submit" class="btn-primary-login">Save</button>
-        </form>
-        <br/><br/><br/><br/>
-        <h3 class="text-center" style="border-bottom: 1px solid #d4d3d3; padding-bottom: 25px">Password</h3>
-        <form action="" method="post">
-            <br/>
-            <div class="order-label">
-                <label for="Current Password"">Current Password: <i class="fas fa-key"></i></label>
-                <input required type="password" class="input-responsive" name="current_pass" placeholder="Current Password">
-            </div>
-            <br>
-            <div class="order-label">
-                <label for="New Password"">New Password: <i class="fas fa-key"></i></label>
-                <input required type="password" class="input-responsive" name="new_password" placeholder="New Password">
-            </div>
-            <button type="submit" name="pass_submit" class="btn-primary-login">Save</button>
-        </form>
-    </div>
+        <div class="order-label">
+            <label for="Current Password"">Current Password: <i class="fas fa-key"></i></label>
+            <input required type="password" class="input-responsive" name="current_pass" placeholder="Current Password">
+        </div>
+        <br>
+        <div class="order-label">
+            <label for="New Password"">New Password: <i class="fas fa-key"></i></label>
+            <input required type="password" class="input-responsive" name="new_password" placeholder="New Password">
+        </div>
+        <button type="submit" name="pass_submit" class="btn-primary-login">Save</button>
+    </form>
 </div>
+
 
     <!--Include social section-->
 <?php include 'partial_front/social.php'?>
