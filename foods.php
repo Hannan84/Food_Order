3fb855a2358a0f6ef8adc677d7e3b99e1d52f06e
Hannan84 <hannansarkar84@gmail.com>
2021-10-28 21:39:22 +0600

    midified some line

--- foods.php
+++ foods.php
@@ -43,7 +43,7 @@ include 'partial_front/navbar.php';
                         <h4><?php echo $value['title']?></h4>
                         <p class="food-price"><?php echo $value['price']?> Tk</p>
                         <p class="food-detail">
-                            <?php echo $value['description']?>
+                            <?php echo preg_replace('/((\w+\W*){'.(13).'}(\w+))(.*)/', '${1}', $value['description']);?>
                         </p>
                         <br>
 
